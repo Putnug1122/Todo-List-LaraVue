aff1a9a673d0194b78679f1ac035770197ce7e2b
deta <deta@pop-os.localdomain>
2021-10-24 09:52:11 +0800

    working

---
+++ resources/js/vue/listView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div v-for="(item, i) in items" :key="i">
+            <list-item
+                :item="item"
+                class="item"
+                v-on:itemChanged="$emit('reloadList')"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import listItem from "./listItem.vue";
+export default {
+    props: ["items"],
+    components: {
+        listItem
+    }
+};
+</script>
+
+<style scoped>
+.item {
+    background-color: #e6e6e6;
+    padding: 5px;
+    margin-top: 5px;
+}
+</style>
